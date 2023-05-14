9c783db703bb3590971e0a1d0cd52318fa7e0b2e
Daniel Gross <d@dcgross.com>
2023-05-14 16:05:13 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@ from setuptools import setup, find_namespace_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='python-llm',
-    version='0.1.2',
+    version='0.1.4',
     author='Daniel Gross',
     author_email='d@dcgross.com',
     description='An LLM wrapper for Humans',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_namespace_packages(include=['llm', 'llm.*']),
+    url="https://github.com/danielgross/python-llm",
     classifiers=[
     ],
     python_requires='>=3.6',
